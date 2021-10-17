2f3cf0807cbb2726b1d8a7c988825c4a94f8f787
momo <wdudtlrw78@gmail.com>
2021-10-18 02:15:37 +0900

    feat: light/dark toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello');
+'use strict';
+
+const checkedDark = document.querySelector('#dark');
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener('click', () => {
+    checkedDark.checked
+      ? (document.querySelector('body').classList = 'dark')
+      : (document.querySelector('body').classList = 'light');
+  });
+}
